bb80fe0e7b6694fba764b7eda8af14c2aac091c4
zjun <565061763@qq.com>
2019-03-21 15:06:21 +0800

    refactor

--- lib/flib_eos_sign.dart
+++ lib/flib_eos_sign.dart
@@ -4,21 +4,21 @@ import 'package:flutter/services.dart';
 
 class FlibEosSign {
   static const MethodChannel _channel =
-  const MethodChannel('flutter.fanwe.com/flib_eos_sign');
+      const MethodChannel('flutter.fanwe.com/flib_eos_sign');
 
-  static Future<String> generatePrivateKey() async {
-    return await _channel.invokeMethod('generatePrivateKey');
+  static Future<String> generatePrivateKey() {
+    return _channel.invokeMethod<String>('generatePrivateKey');
   }
 
-  static Future<String> toPublicKey(String privateKey) async {
-    return await _channel.invokeMethod('toPublicKey', privateKey);
+  static Future<String> toPublicKey(String privateKey) {
+    return _channel.invokeMethod<String>('toPublicKey', privateKey);
   }
 
-  static Future<String> sign(List<int> data, String privateKey) async {
+  static Future<String> sign(List<int> data, String privateKey) {
     final Map<String, dynamic> arguments = {
       'data': data,
       'privateKey': privateKey,
     };
-    return await _channel.invokeMethod('sign', arguments);
+    return _channel.invokeMethod<String>('sign', arguments);
   }
 }
